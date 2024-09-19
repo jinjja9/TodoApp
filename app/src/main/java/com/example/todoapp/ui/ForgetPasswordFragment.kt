4a2dac8d3b5dbcc9102487f5de8a472b9719b276
jinjja9 <hieulc2012004@example.com>
2024-09-19 17:53:33 +0700

    add UI home

---
+++ app/src/main/java/com/example/todoapp/ui/ForgetPasswordFragment.kt
@@ -0,0 +1,66 @@
+package com.example.todoapp.ui
+
+import android.os.Bundle
+import android.text.InputType
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentForgetPasswordBinding
+
+
+class ForgetPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentForgetPasswordBinding
+    private var isPasswordVisible = false
+    private var isConfirmPasswordVisible = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentForgetPasswordBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.left.setOnClickListener {
+            findNavController().navigate(R.id.action_forgetPasswordFragment_to_signInFragment)
+        }
+
+        binding.imageViewEye.setOnClickListener {
+            togglePasswordVisibility(binding.editpassword, binding.imageViewEye, isPasswordVisible)
+            isPasswordVisible = !isPasswordVisible
+        }
+
+        binding.imageViewEye1.setOnClickListener {
+            togglePasswordVisibility(
+                binding.editrpassword,
+                binding.imageViewEye1,
+                isConfirmPasswordVisible
+            )
+            isConfirmPasswordVisible = !isConfirmPasswordVisible
+        }
+
+    }
+    private fun togglePasswordVisibility(editText: EditText, imageView: ImageView, isVisible: Boolean)
+    {
+        if(isVisible)
+        {
+            editText.inputType=InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            imageView.setImageResource(R.drawable.eye_off)
+        }
+        else {
+            editText.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            imageView.setImageResource(R.drawable.eye_on)
+        }
+        editText.setSelection(editText.text.length)
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/ui/SignInFragment.kt
@@ -0,0 +1,60 @@
+package com.example.todoapp.ui
+
+import android.os.Bundle
+import android.text.InputType
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
+    private var isPasswordVisible = false
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textsignup.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+        binding.textforgetpassword.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_forgetPasswordFragment)
+        }
+        binding.buttonsignin.setOnClickListener{
+            findNavController().navigate(R.id.action_signInFragment_to_homeScreenFragment)
+        }
+
+        // An-Hien Pass
+        binding.imageViewEye.setOnClickListener {
+            togglePasswordVisibility()
+        }
+    }
+    private fun togglePasswordVisibility() {
+        if (isPasswordVisible) {
+            // Ẩn
+            binding.editpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            binding.imageViewEye.setImageResource(R.drawable.eye_off)
+        } else {
+            // Hiển
+            binding.editpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            binding.imageViewEye.setImageResource(R.drawable.eye_on)
+        }
+        // Di chuyen con tro đen cuối văn bản
+       binding.editpassword.setSelection(binding.editpassword.text.length)
+        isPasswordVisible = !isPasswordVisible
+    }
+
+}
